b1123455444422260449503a228400ad38857dbb
adeetshah <adeet.shah@ge.com>
2014-04-18 12:52:26 -0700

    added easy navigation on report form from one section to another

--- public/javascripts/chaya.js
+++ public/javascripts/chaya.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$("#first_time").hide();
 	$("#other_div").hide();
-
 });
 
 
@@ -21,4 +20,46 @@ function first_time_display()
 function describe_Incident()
 {
 	$("#other_div").show();
+}
+
+function onSubmitClick() //to check for empty fields
+{
+	flag = true;
+	onSubmission();	
+}
+
+function onSubmission()
+{
+	var person = $('input[name=person]:checked').val(); //person who is reporting
+	
+	var doYouKnow = $('input[name=assailant]:checked').val(); //do you know assailant
+	
+	var firstTimeCrime = $('input[name=time]:checked').val(); //first time facing crime
+	
+	var incident_list="";
+	 $('.incident:checked').each(function() {
+          incident_list += $(this).val() + ",";
+        });
+        incident_list =  incident_list.slice(0,-1);
+    	//alert(incident_list);
+    
+    var incident_description="";
+     
+    if(incident_list.indexOf("Other")!=-1)
+    	incident_description = $("textarea#other").val();
+ 	
+ 	//alert(incident_description);
+ 	
+	var location = $("#location").val();
+
+	var date = $("#date").val();
+	var time = $("#time").val();
+	
+ 	var comments = $("textarea#additional_info").val();
+ 	
+ 	var firstName = $("#firstName").val();
+ 	var lastName = $("#lastName").val();
+ 	var email = $("#email").val();
+ 	var number = $("#number").val();
+ 	
 }
